feb7b0c0390100671da1024ceacdb3c7d296b6cf
Ashika Anand Babu <Ashika.makam@gmail.com>
2023-08-21 12:13:56 -0700

    Update CountriesDetails.js

--- src/components/CountriesDetails/CountriesDetails.js
+++ src/components/CountriesDetails/CountriesDetails.js
@@ -8,36 +8,35 @@ const CountriesDetails = () => {
   const [mapData, setMapData] = useState(data);
   const [country, setCountry] = useState({});
   const { countryname } = useParams();
-  const [news, setNews] = useState([]); // [ { title: "", description: "", url: "" } ]
+  // const [news, setNews] = useState([]); // [ { title: "", description: "", url: "" } ]
 
   useEffect(() => {
-    const fetchNews = async (countryCode) => {
-      try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
-        const newsData = await response.json();
-        console.log(newsData);
-        setNews(newsData.articles);
-      } catch (error) {
-        console.error("Error fetching news:", error);
-      }
-    }
+    // const fetchNews = async (countryCode) => {
+    //   try {
+    //     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
+    //     const newsData = await response.json();
+    //     console.log(newsData);
+    //     setNews(newsData.articles);
+    //   } catch (error) {
+    //     console.error("Error fetching news:", error);
+    //   }
+    // }
     const fetchCountry = async () => {
       try {
         const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryname}`);
         const countriesData = await response.json();
-        console.log(countriesData);
+        // console.log(countriesData);
         if (countriesData && countriesData.length > 0) {
           const countryData = countriesData[0];
           setCountry(countryData);
           setMapData([{ country: countryData.cca2, value: countryData.population }]);
-          // fetchNews(countryData.cca2);
         }
       } catch (error) {
         console.error("Error fetching country:", error);
       }
     };
     fetchCountry();
-    fetchNews(countryname);
+    // fetchNews(countryname);
   }, [countryname]);
 
   return (
@@ -91,25 +90,25 @@ const CountriesDetails = () => {
         </div>
       </div>
 
-      {news.length > 0 && (
-        <div className="mt-4 border border-blue rounded m-5 pt-5">
-        <div className="text-gray-800 text-xl font-semibold mb-4">Today's Top News</div>
-        {news.map((article, index) => (
-          <div key={index} className="border border-gray-600 p-4 mb-4 rounded m-10">
-            <p className="text-gray-800 text-lg font-semibold mb-2">{article.title}</p>
-            <p className="text-gray-600 text-sm mb-2">{article.description}</p>
-            <a
-              href={article.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white border border-white rounded py-1 px-3 bg-blue-500 hover:bg-blue-900 hover:text-white"
-            >
-              Read More
-            </a>
-          </div>
-        ))}
-      </div>
-      )}
+      // {news.length > 0 && (
+      //   <div className="mt-4 border border-blue rounded m-5 pt-5">
+      //   <div className="text-gray-800 text-xl font-semibold mb-4">Today's Top News</div>
+      //   {news.map((article, index) => (
+      //     <div key={index} className="border border-gray-600 p-4 mb-4 rounded m-10">
+      //       <p className="text-gray-800 text-lg font-semibold mb-2">{article.title}</p>
+      //       <p className="text-gray-600 text-sm mb-2">{article.description}</p>
+      //       <a
+      //         href={article.url}
+      //         target="_blank"
+      //         rel="noopener noreferrer"
+      //         className="text-white border border-white rounded py-1 px-3 bg-blue-500 hover:bg-blue-900 hover:text-white"
+      //       >
+      //         Read More
+      //       </a>
+      //     </div>
+      //   ))}
+      // </div>
+      // )}
 
     </>
   );
